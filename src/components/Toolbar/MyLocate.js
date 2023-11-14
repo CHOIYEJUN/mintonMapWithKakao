25100c97e6aaf48d70c0d35ba2b82cb33a5b35c6
최예준 <dpwns108@naver.com>
2023-11-14 17:30:51 +0900

    카카오맵 검색기능 핀 이미지 수정 및 스타일 수정하여 커밋

--- src/components/Toolbar/MyLocate.js
+++ src/components/Toolbar/MyLocate.js
@@ -4,13 +4,43 @@ import {BiCurrentLocation} from "react-icons/bi";
 
 
 export default function MyLocate () {
+    const MarkerArr = [];
 
-
-
+    const imageSize = new window.kakao.maps.Size(30, 30); // 마커이미지의 크기
+    const imgSrc ='https://t1.daumcdn.net/localimg/localimages/07/2018/mw/m640/ico_marker.png';
 
     const onClick = () => {
         const _map = window.kakaoMap;
-        _map.setLevel(_map.getLevel() - 1);
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                const lat = position.coords.latitude;
+                const lng = position.coords.longitude;
+                _map.setCenter(new window.kakao.maps.LatLng(lat, lng));
+                deleteMarkers(MarkerArr);
+                const markerImage = new window.kakao.maps.MarkerImage(
+                    imgSrc,
+                    imageSize
+                );
+                const locPosition  = new window.kakao.maps.Marker({
+                    position: new window.kakao.maps.LatLng(lat, lng),
+                    map: _map,
+                    image : markerImage
+                });
+                MarkerArr.push(locPosition);
+            });
+        } else {
+            alert("GPS를 지원하지 않습니다.");
+        }
+
+        const deleteMarkers = (markers) => {
+            if(markers.length === 0) return;
+
+            for (let i = 0; i < markers.length; i++) {
+                markers[i].setMap(null);
+            }
+            markers = [];
+        };
+
 
     }
     return (
