a50f1f6c7b83d03a9f361d2563720b33352754bd
최예준 <dpwns108@naver.com>
2023-11-15 11:02:09 +0900

    보안처리

--- src/fireBase.js
+++ src/fireBase.js
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore"; // 추가
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBTd-XMVz-A2tUUKPFDOkNFUjd44QVVjsU",
-    authDomain: "mintonmap-7f2e8.firebaseapp.com",
-    projectId: "mintonmap-7f2e8",
-    storageBucket: "mintonmap-7f2e8.appspot.com",
-    messagingSenderId: "305256909429",
-    appId: "1:305256909429:web:d5bc27201ee07e4ff5063c"
+    apiKey: process.env.REACT_APP_APP_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
