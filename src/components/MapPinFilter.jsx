cf5d9806765e030a360664202702bcd91d523481
최예준 <dpwns108@gmail.com>
2024-02-10 10:45:20 +0900

    맵필터 스타일 적용중

--- src/components/MapPinFilter.jsx
+++ src/components/MapPinFilter.jsx
@@ -1,4 +1,14 @@
-import {Box, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, Text} from "@chakra-ui/react";
+import {
+    Box,
+    Drawer,
+    DrawerBody,
+    DrawerCloseButton,
+    DrawerContent,
+    DrawerHeader,
+    DrawerOverlay,
+    Select,
+    Text
+} from "@chakra-ui/react";
 import {useRecoilState} from "recoil";
 import {MapPinFilterIsOpenState} from "../states/MapStates";
 import {useEffect} from "react";
@@ -17,15 +27,37 @@ export default function MapPinFilter () {
           isOpen={isOpen}
           onClose={onClose}
           placement="right"
-          size={'md'}
+          size={'sm'}
         >
             <DrawerOverlay />
             <DrawerContent>
                 <DrawerCloseButton />
-                <DrawerHeader>필터</DrawerHeader>
+                <DrawerHeader>지도 필터</DrawerHeader>
                 <DrawerBody>
                 <Box>
-                    <Text>필터</Text>
+                    <Text>운영기관</Text>
+                    <Select placeholder="운영기관">
+                        <option value="all">전체</option>
+                        <option value="national">국립</option>
+                        <option value="private">사립</option>
+                    </Select>
+
+                    <Text>운영중</Text>
+                    <Select placeholder="운영">
+                        <option value="all">전체</option>
+                        <option value="Y">운영중</option>
+                        <option value="N">마감</option>
+                    </Select>
+
+                    <Text>내위치 기준 반경</Text>
+                    <Select placeholder="반경">
+                        <option value="all">전체</option>
+                        <option value="1">1km</option>
+                        <option value="3">3km</option>
+                        <option value="5">5km</option>
+                    </Select>
+
+
                 </Box>
                 </DrawerBody>
             </DrawerContent>
